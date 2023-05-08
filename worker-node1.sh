e435d327234fcc5231fc3a64cc717c5ce0c4457f
Muhammad Omar Riaz <40802884+muhammad-omar-riaz@users.noreply.github.com>
2023-05-08 14:01:00 +0500

    Update worker-node1.sh

--- worker-node1.sh
+++ worker-node1.sh
@@ -265,6 +265,10 @@ echo "50%"
 dnf install -y kubelet kubeadm kubectl --disableexcludes=kubernetes >/dev/null 2>&1
 
 echo "75%"
+
+rm -rf /etc/containerd/config.toml
+systemctl restart containerd
+
 echo "100%"
 echo "Completed"
 echo
